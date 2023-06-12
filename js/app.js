76cd0599b83c2909770cdd0cffac329bbde5fe35
Chorro Michael <michaelchorro.dev@gmail.com>
2023-06-12 15:30:30 +0200

    remove meal & workout

--- js/app.js
+++ js/app.js
@@ -30,6 +30,28 @@ class CalorieTracker {
     this._render();
   }
 
+  removeMeal(id) {
+    const index = this._meals.findIndex((meal) => meal.id === id);
+
+    if (index !== -1) {
+      const meal = this._meals[index];
+      this._totalCalories -= meal.calories;
+      this._meals.splice(index, 1);
+      this._render();
+    }
+  }
+
+  removeWorkout(id) {
+    const index = this._workouts.findIndex((workout) => workout.id === id);
+
+    if (index !== -1) {
+      const workout = this._workouts[index];
+      this._totalCalories += workout.calories;
+      this._workouts.splice(index, 1);
+      this._render();
+    }
+  }
+
   // PRIVATE METHODS //
   _displayCaloriesTotal() {
     const totalCaloriesEl = document.getElementById('calories-total');
@@ -173,7 +195,7 @@ class Workout {
 
 class App {
   constructor() {
-    this.tracker = new CalorieTracker();
+    this._tracker = new CalorieTracker();
 
     document
       .getElementById('meal-form')
@@ -181,6 +203,14 @@ class App {
     document
       .getElementById('workout-form')
       .addEventListener('submit', this._newItem.bind(this, 'workout'));
+
+    document
+      .getElementById('meal-items')
+      .addEventListener('click', this._removeItem.bind(this, 'meal'));
+
+    document
+      .getElementById('workout-items')
+      .addEventListener('click', this._removeItem.bind(this, 'workout'));
   }
 
   _newItem(type, e) {
@@ -195,11 +225,11 @@ class App {
 
     if (type === 'meal') {
       const meal = new Meal(name.value, +calories.value);
-      this.tracker.addMeal(meal);
+      this._tracker.addMeal(meal);
     } else {
       const workout = new Workout(name.value, +calories.value);
 
-      this.tracker.addWorkout(workout);
+      this._tracker.addWorkout(workout);
     }
 
     name.value = '';
@@ -209,6 +239,23 @@ class App {
     const bsCollapse = new bootstrap.Collapse(collapseItem, {
       toggle: true,
     });
+  }
+
+  _removeItem(type, e) {
+    if (
+      e.target.classList.contains('delete') ||
+      e.target.classList.contains('fa-xmark')
+    ) {
+      if (confirm('Are you sure')) {
+        const id = e.target.closest('.card').getAttribute('data-id');
+
+        type === 'meal'
+          ? this._tracker.removeMeal(id)
+          : this._tracker.removeWorkout(id);
+
+        e.target.closest('.card').remove();
+      }
+    }
   }
 }
 
